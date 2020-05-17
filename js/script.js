c64d4c90f19240139cf5688e72a52a164e5fca52
Andrey Tsivtsivadze <andrey.zonter@yandex.ru>
2020-05-17 23:34:39 +0300

    Add popup and burger-menu

--- js/script.js
+++ js/script.js
@@ -1,40 +1,67 @@
 $(document).ready(function(){
   $('.about__symbol').addClass('about__symbol--active');
 
- new Swiper('.slider', {
-  slidesPerView: 1,
-  spaceBetween: 25,
-  breakpoints: {
-    768: {
-      slidesPerView: 2
-    },
-    1350: {
-      slidesPerView: 3,
-      spaceBetween: 30
+  $('.header__burger').click(function () {
+    $(this).toggleClass('open');
+    $('.header__menu').toggleClass('header__menu--burger');
+  });
+
+    $(".for-anchor").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 800);
+    });
+
+  $('.button-orange, .button-transparent, .popup-close').click(function () {
+    $('.popup__window').fadeToggle();
+    $('body').toggleClass('scrollOff');
+    $('header, main, footer').toggleClass('blur');
+  });
+
+  $(document).mouseup(function (e) {
+    var div = $(".popup__window");
+    if (!div.is(e.target)
+      && div.has(e.target).length === 0) {
+      div.fadeOut();
+      $('body').removeClass('scrollOff');
+      $('header, main, footer').removeClass('blur');
     }
-  },
-  loop: true,
-  wrapperClass: 'slider__list',
-  slideClass: 'slider__item',
-  pagination: {
-    el: '.slider__pagination',
-    type: 'bullets',
-    bulletClass: 'paginator__item',
-    bulletActiveClass: 'paginator__item--active',
-    clickable: true
-  },
-  navigation: {
-    nextEl: '.slider__button--next',
-    prevEl: '.slider__button--prev',
-  },
-  a11y: {
-    prevSlideMessage: 'Предыдущий слайд',
-    nextSlideMessage: 'Следующий слайд',
-    firstSlideMessage: 'Первый слайд',
-    lastSlideMessage: 'Последний слайд',
-    paginationBulletMessage: 'Перейти к слайду {{index}}',
-  },
-});
+  });
+
+  new Swiper('.slider', {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    breakpoints: {
+      768: {
+        slidesPerView: 2
+      },
+      1350: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    loop: true,
+    wrapperClass: 'slider__list',
+    slideClass: 'slider__item',
+    pagination: {
+      el: '.slider__pagination',
+      type: 'bullets',
+      bulletClass: 'paginator__item',
+      bulletActiveClass: 'paginator__item--active',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.slider__button--next',
+      prevEl: '.slider__button--prev',
+    },
+    a11y: {
+      prevSlideMessage: 'Предыдущий слайд',
+      nextSlideMessage: 'Следующий слайд',
+      firstSlideMessage: 'Первый слайд',
+      lastSlideMessage: 'Последний слайд',
+      paginationBulletMessage: 'Перейти к слайду {{index}}',
+    },
+  });
 
 });
-
